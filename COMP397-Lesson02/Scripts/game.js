018ea8f50bdd456c3bc50c792e1b1d372fa04cea
iloy1004 <iloy1004ca@gmail.com>
2015-01-12 13:39:33 -0500

    created basic site structure for boilerplate

---
+++ COMP397-Lesson02/Scripts/game.js
@@ -0,0 +1,15 @@
+﻿var Player = (function () {
+    function Player() {
+        this.strength = 10;
+    }
+    Player.prototype.fights = function () {
+        console.log("He Fights");
+    };
+    return Player;
+})();
+
+function main() {
+    var tom = new Player();
+    tom.fights();
+}
+//# sourceMappingURL=game.js.map
